961dfad33be346e22e7b3e6d8aa1261acc2a6eaf
meghana361 <meghanamaggie0987@gmail.com>
2024-07-12 12:21:39 +0530

    First COmmit rural

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,7 @@
+
 const mongoose = require("mongoose")
+const dotenv = require('dotenv');
+dotenv.config();
 const mongoDbUrl=process.env.MONGO_URI;
 const connectDb=()=>{
     return mongoose.connect(mongoDbUrl)
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,36 @@
 const { app } = require(".");
 const { connectDb } = require("./config/db");
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const url = require('url'); 
+const { fileURLToPath } =require('url')
+const{dirname} =require("path")
+const cors = require('cors');
+app.use(cors());
+dotenv.config();
 
-const PORT = 5454;
+const PORT=process.env.PORT || 5000;
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// if(process.env.NODE_ENV==='production'){
+//   app.use(express.static(path.join(__dirname,"/frontend/dist")))
+// }
+// app.get("*",(req,res)=>{
+//   res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+// })
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+}
 app.listen(PORT, async () => {
   await connectDb();
   console.log(`server running at port http://localhost:${PORT}`);
   console.log("connected to mongodb");
+
 });
